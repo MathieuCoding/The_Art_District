121a1d75bee093be8cd20ec685674f5f501009e9
MathieuCoding <polletmathieu.pro@gmail.com>
2023-05-16 18:34:44 +0200

    update

--- views/indexView.php
+++ views/indexView.php
@@ -2,33 +2,6 @@
 require_once 'partials/header.php';
 ?>
 
-
-<!-- Carousel -->
-
-<!-- <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="images\pexels-jacob-colvin-1761282.jpg" class="d-block w-25 img-fluid" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="images\pexels-pixabay-41006.jpg" class="d-block w-25 img-fluid" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="images\pexels-pixabay-208821.jpg" class="d-block w-25 img-fluid" alt="...">
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div> -->
-
-
-
 <!-- Here comes the cards -->
 
 <div class="container px-4 py-5" id="custom-cards">
